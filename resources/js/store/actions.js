4296e1148904fe245f39a4e7c03621c86f0e0a97
Ajdin <ajdin@ceraenterprises.com>
2021-06-28 02:08:10 +0200

    changed route:

--- resources/js/store/actions.js
+++ resources/js/store/actions.js
@@ -2,7 +2,7 @@ let actions = {
     ADD_ORDER({
         commit
     }, order) {
-       axios.post('/api/orders222', order).then(res => {
+       axios.post('/api/orders', order).then(res => {
         if (res.data === "added")
             commit("ADD_ORDER", order);
             return order;
